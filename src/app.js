30af61bfd11fec5a798d25a1a767c223e441f200
James Triantafylos <jtriantafylos@gmail.com>
2020-01-11 14:20:25 -0500

    Created visualizer web server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
+// Read the dataset into memory
 const fs = require('fs')
+const dataSet = JSON.parse(fs.readFileSync('../data/data.json'))
 
+// Scrape each event type from the dataset
+const eventTypes = []
+for (const timestamp in dataSet) {
+  if (!eventTypes.includes(dataSet[timestamp].event)) {
+    eventTypes.push(dataSet[timestamp].event)
+  }
+}
+
+// Replace epoch timestamps with a user-readable format in the dataset
+for (const timestamp in dataSet) {
+  dataSet[new Date(timestamp * 1000).toLocaleString()] = dataSet[timestamp]
+  delete dataSet[timestamp]
+}
+
+// Visualizer web server setup
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Visualizer web server routes
+app.get('/', express.static('public'))
+app.get('/data', (req, res) => {
+  res.json(dataSet)
+})
+
+app.listen(port, () => console.log('Visualizer web server started!'))
 
