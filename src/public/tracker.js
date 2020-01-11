5c80fbaf8f136c2ecb9a1bf63fdf38a62c0c7fd2
Eyas Valdez <eyasvaldez7@gmail.com>
2020-01-11 14:56:15 -0500

    working on tracker

--- src/public/tracker.js
+++ src/public/tracker.js
@@ -1,10 +1,21 @@
 var myGamePiece;
+const RED = '#ff2915';
+const BLUE = '#2925ff';
+const YELLOW = '#ffd000';
+const GREEN = '#14ab03';
+const VIOLET = '#7f25b0';
+const ORANGE = '#e38f28';
+const BLACK = '#000000';
+const GRAY = '#707070';
+const PINK = '#f280e3';
+
 class person{
-    constructor(name, role, room, position){
+    constructor(name, role, room, position, colour){
         this.name = name;
         this.role = role;
         this.room = room;
         this.position = position;
+        this.colour = colour;
 
     }
     getName(){
@@ -19,137 +30,86 @@ class person{
     getPosition(){
         return this.position;
     }
+    getColour(){
+        return this.colour;
+    }
     updatePosition(newP){
         this.position = newP;
     }
 }
 
-var myGamePiece;
-var myObstacles = [];
-var myScore;
+let people;
+
 
 function startVisualizer() {
     //initializing guests
-    let veronica = new person('Veronica', 'guest', 210, [0,0]);
-    let jason = new person('Jason', 'guest', 241, [0,0]);
-    let thomas = new person('Thomas', 'guest', 248, [0,0]);
-    let rob = new person('Rob', 'guest', 231, [0,0]);
-    let kristina = new person('Kristina', 'guest', 235, [0,0]);
+    let veronica = new person('Veronica', 'guest', 210, [0,0], RED);
+    let jason = new person('Jason', 'guest', 241, [0,0], BLUE);
+    let thomas = new person('Thomas', 'guest', 248, [0,0], ORANGE);
+    let rob = new person('Rob', 'guest', 231, [0,0], YELLOW);
+    let kristina = new person('Kristina', 'guest', 235, [0,0], BLACK);
 
     //initializing staff
-    let marc = new person('Marc-Andre', 'cleaning', 0, [0,0]);
-    let dave = new person('Dave', 'cooking', 0, [0,0]);
-    let salina = new person('Salina', 'reception', 0, [0,0]);
-    let harrison = new person('Harrison', 'reception late-night', 0, [0,0]);
+    let marc = new person('Marc-Andre', 'cleaning', 0, [0,0], VIOLET);
+    let dave = new person('Dave', 'cooking', 0, [0,0], PINK);
+    let salina = new person('Salina', 'reception', 0, [0,0], GRAY);
+    let harrison = new person('Harrison', 'reception late-night', 0, [0,0], GREEN);
 
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece.gravity = 0.05;
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    people = [veronica, jason, thomas, rob, kristina, marc, dave, salina, harrison];
+
+
     simulationArea.start();
 }
 
 var simulationArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 785;
+        this.canvas.height = 857;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateSimulation, 20);
         },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.score = 0;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;
-    this.gravity = 0;
-    this.gravitySpeed = 0;
-    this.update = function() {
-        ctx = simulationArea.context;
-        if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
-            ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-    this.newPos = function() {
-        this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }
-    this.hitBottom = function() {
-        var rockbottom = simulationArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
-        }
-    }
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
-        }
-        return crash;
-    }
-}
+// function component(width, height, color, x, y, type) {
+//     this.type = type;
+//     this.score = 0;
+//     this.width = width;
+//     this.height = height;
+//     this.speedX = 0;
+//     this.speedY = 0;    
+//     this.x = x;
+//     this.y = y;
+//     this.gravity = 0;
+//     this.gravitySpeed = 0;
+//     this.update = function() {
+//         ctx = simulationArea.context;
+//         if (this.type == "text") {
+//             ctx.font = this.width + " " + this.height;
+//             ctx.fillStyle = color;
+//             ctx.fillText(this.text, this.x, this.y);
+//         } else {
+//             ctx.fillStyle = color;
+//             ctx.fillRect(this.x, this.y, this.width, this.height);
+//         }
+//     }
+    
+    
+// }
 
-function updateGameArea() {
-    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < myObstacles.length; i += 1) {
-        if (myGamePiece.crashWith(myObstacles[i])) {
-            return;
-        } 
-    }
+function updateSimulation() {
+    
     simulationArea.clear();
     simulationArea.frameNo += 1;
-    if (simulationArea.frameNo == 1 || everyinterval(150)) {
-        x = simulationArea.canvas.width;
-        minHeight = 20;
-        maxHeight = 200;
-        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-        minGap = 50;
-        maxGap = 200;
-        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "green", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
-    }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
-    }
-    myScore.text="SCORE: " + simulationArea.frameNo;
-    myScore.update();
-    myGamePiece.newPos();
-    myGamePiece.update();
+    
+    
+    
 }
 
-function everyinterval(n) {
-    if ((simulationArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
-}
 
-function accelerate(n) {
-    myGamePiece.gravity = n;
-}
+
