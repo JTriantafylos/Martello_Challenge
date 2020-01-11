69ce3f07c793ffd636c71ef7c2a72ce65c62157c
James Triantafylos <jtriantafylos@gmail.com>
2020-01-11 13:16:05 -0500

    First Commit

--- src/app.py
+++ src/app.py
@@ -1,16 +1,1 @@
-import json
-
-# Load the dataset into memory
-dataFile = open('../data/data.json', 'r')
-dataSet = json.load(dataFile)
-
-dataSetEvents = []
-
-# Scrape event types from dataset
-for data in dataSet:
-    if dataSet[data]['event'] not in dataSetEvents:
-        dataSetEvents.append(dataSet[data]['event'])
-    pass
-
-print(dataSetEvents)
-
+# EMPTY
